15086d9435878a8c6202d3993e1ff6cab1d15eb5
sooyaaabo <76434723+sooyaaabo@users.noreply.github.com>
2023-08-13 11:14:01 +0800

    Update aliyunsvip.js

--- aliyunsvip.js
+++ aliyunsvip.js
@@ -9,7 +9,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/(api|member)\.(aliyundrive|alipan)\.com\/(.+\/(users|activity|user\/get)|((business|apps)\/.+\/users|adrive\/.+\/user)) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/alidrive.js
+^https?:\/\/(api|member)\.(aliyundrive|alipan)\.com\/(.+\/(users|activity|user\/get)|((business|apps)\/.+\/users|adrive\/.+\/user)) url script-response-body https://raw.githubusercontent.com/sooyaaabo/loon/main/aliyunsvip.js
 
 [mitm]
 hostname = *.aliyundrive.com, *.alipan.com
