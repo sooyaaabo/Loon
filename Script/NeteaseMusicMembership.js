958a383f4af6568fef0a0254f0a17fb65f14c88c
恩秀 <3332029329@qq.com>
2024-10-07 15:52:36 +0800

    更新

---
+++ Script/NeteaseMusicMembership.js
@@ -0,0 +1,23 @@
+// 2024-08-05 19:25
+
+const url = $request.url;
+const header = $request.headers;
+const isNetEase = url.includes("/interface") && url.includes(".music.163.com/");
+
+if (isNetEase) {
+  if ($persistentStore.read("开启共享会员") === "是") {
+    if ($persistentStore.read("Music163_MConfigInfo") === undefined || $persistentStore.read("Music163_MConfigInfo") === null) {
+        $notification.post("脚本出错", "参数缺失", "请在插件内填入数据");
+        $done({});
+      } else {
+        header["cookie"] = $persistentStore.read("Music163_Cookie");
+        header["mconfig-info"] = $persistentStore.read("Music163_MConfigInfo");
+        header["user-agent"] = $persistentStore.read("Music163_UserAgent");
+      }
+    } else {
+      $done({});
+  }
+  $done({ headers: header });
+} else {
+  $done({});
+}
