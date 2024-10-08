4fd88fa52586adeb14e23f757ca224c28e01e7c1
恩秀 <3332029329@qq.com>
2024-10-08 09:22:32 +0800

    更新

--- Script/NeteaseMusicMembership.js
+++ Script/NeteaseMusicMembership.js
@@ -1,22 +1,22 @@
-// 2024-08-05 19:25
+// 2024-10-08 02:15
 
 const url = $request.url;
 const header = $request.headers;
 const isNetEase = url.includes("/interface") && url.includes(".music.163.com/");
 
 if (isNetEase) {
-  if ($persistentStore.read("开启共享会员") === "是") {
-    if ($persistentStore.read("Music163_MConfigInfo") === undefined || $persistentStore.read("Music163_MConfigInfo") === null) {
-        $notification.post("脚本出错", "参数缺失", "请在插件内填入数据");
-        $done({});
-      } else {
-        header["cookie"] = $persistentStore.read("Music163_Cookie");
-        header["mconfig-info"] = $persistentStore.read("Music163_MConfigInfo");
-        header["user-agent"] = $persistentStore.read("Music163_UserAgent");
-      }
-    } else {
-      $done({});
+  if (
+    $persistentStore.read("Music163_MConfigInfo") === undefined ||
+    $persistentStore.read("Music163_MConfigInfo") === null
+  ) {
+    $notification.post("网易云音乐遇到问题", "参数缺失", "请在插件内填入会员数据");
+    $done({});
+  } else {
+    header["cookie"] = $persistentStore.read("Music163_Cookie");
+    header["mconfig-info"] = $persistentStore.read("Music163_MConfigInfo");
+    header["user-agent"] = $persistentStore.read("Music163_UserAgent");
   }
+
   $done({ headers: header });
 } else {
   $done({});
